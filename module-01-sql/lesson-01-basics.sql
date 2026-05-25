bc2b14eae9aa8f80692c10e098ea352f89bb95d6
Nabiya Maredia <nabiyamaredia@macbookpro.lan>
2026-05-25 15:44:29 -0500

    adding order by and limit queries

--- module-01-sql/lesson-01-basics.sql
+++ module-01-sql/lesson-01-basics.sql
@@ -43,3 +43,21 @@ from tickets
 where status = 'Closed'
 group BY priority
 HAVING AVG(resolution_hours) > 5
+
+SELECT ticket_id, priority, resolution_hours
+from tickets
+ORDER by resolution_hours DESC;
+
+select ticket_id, priority, resolution_hours
+FROM tickets
+WHERE status = 'Closed'
+ORDER by resolution_hours ASC
+limit 2;
+
+-- flawed query, because it doesn't consider the priority levels correctly. In real life scenarios, you would need to assign numerical values to the priority levels (e.g., High = 1, Medium = 2, Low = 3) and then order by those values to get the correct highest priority ticket. However, for the sake of this exercise, we will assume that the alphabetical order of the priority levels is sufficient. This would definately break the logic in real life scenarios, but it serves the purpose of demonstrating the use of ORDER BY and LIMIT clauses in SQL.
+SELECT ticket_id, priority, resolution_hours
+from tickets
+WHERE  status = 'Open'
+order by priority ASC
+LIMIT 1;
+
